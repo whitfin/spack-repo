aa8198a90a0381b8bc0d53110e003611fc922c9c
Isaac Whitfield <iw@whitfin.io>
2019-06-05 15:07:15 -0700

    Remove packages which have been added to mainstream

--- packages/libimobiledevice/package.py
+++ packages/libimobiledevice/package.py
@@ -6,7 +6,7 @@
 from spack import *
 
 
-class Libimobiledevice(Package):
+class Libimobiledevice(AutotoolsPackage):
     """Library to communicate with iOS devices natively."""
 
     homepage = "https://www.libimobiledevice.org/"
@@ -14,31 +14,21 @@ class Libimobiledevice(Package):
     git      = "https://git.libimobiledevice.org/libimobiledevice.git"
 
     version('master', branch='master')
-    version('1.2.0',  sha256='786b0de0875053bf61b5531a86ae8119e320edab724fc62fe2150cc931f11037')
 
-    depends_on('autoconf',   type='build', when='@master')
-    depends_on('automake',   type='build', when='@master')
-    depends_on('libtool',    type='build', when='@master')
-    depends_on('pkg-config', type='build')
+    depends_on('autoconf',     type='build', when='@master')
+    depends_on('automake',     type='build', when='@master')
+    depends_on('libtool',      type='build', when='@master')
+    depends_on('pkgconfig',    type='build')
+
     depends_on('libplist')
     depends_on('libtasn1')
     depends_on('libusbmuxd')
     depends_on('openssl')
 
-    phases = ['autogen', 'install']
-
-    def setup_environment(self, spack_env, run_env):
-        spack_env.append_flags('LDFLAGS', self.spec['openssl'].libs.search_flags)
-
-    def autogen(self, spec, prefix):
-        if self.spec.satisfies('@master'):
-            autogen = Executable('./autogen.sh')
-            autogen()
-
-    def install(self, spec, prefix):
-        configure('--disable-dependency-tracking',
-                  '--disable-silent-rules',
-                  '--enable-debug-code',
-                  '--prefix=%s' % self.spec.prefix,
-                  '--without-cython')
-        make('install')
+    def configure_args(self):
+        return [
+            '--disable-dependency-tracking',
+            '--disable-silent-rules',
+            '--enable-debug-code',
+            '--without-cython'
+        ]
